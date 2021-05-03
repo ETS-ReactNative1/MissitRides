dd9a908fc331dbd0b53c49fd6fdca3f911b3932b
nbhatialin <nbhatialin@gmail.com>
2021-05-03 16:58:24 -0400

    accepted changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -72,49 +72,6 @@ export default class Home extends React.Component {
     else{
       // console.log(markers);  
 
-<<<<<<< HEAD
-      markers.map((marker) => (marker == null ? null : marker.distance = this.getDistance(marker.latlong)));
-      markers.sort(this.compareDistance);
-      this.setState({markers: markers, pickup: this.state.pickup == null ? markers[0]: this.state.pickup})
-    }
-  }
-  async getNearbyPlaces(){
-    this.setState({markers: null});
-    var userid = 1;
-    var lat = this.state.locationResult["coords"]["latitude"];
-    var long = this.state.locationResult["coords"]["longitude"];
-    var req_string = "https://missit-ridesapi-backend.ue.r.appspot.com/fetch_places?userid=" + userid + "&location=" + lat + "," + long
-    // var req_string = "https://missit-ridesapi-backend.ue.r.appspot.com/fetch_places?userid=1&location=52.2075,0.146521"
-    console.log(req_string);
-    fetch(req_string, {
-      
-      method: "POST", 
-              
-      body: null
-    })
-    .then(response => response.json()) 
-
-    .then(json => this.handleNearby(json))
-  }
-      
-  async handleNearby(data) {
-    for(var i = 0; i < data.length; i++){ 
-      data[i].latlong = {latitude:  data[i]["lat"], longitude: data[i]["lng"]}
-      data[i].distance = this.getDistance(data[i].latlong);
-      data.favorite = false
-      }      
-    data.sort(this.compareDistance);
-    // console.log(data);
-    this.setState({markers: data});
-    // console.log(data);
-    try {
-      const jsonValue = JSON.stringify(data)
-      await AsyncStorage.setItem("markers", jsonValue);
-    } catch (e) {
-      // saving error
-    }
-  } 
-=======
   async setup() {
     await getLocationAsync();
     await initializeFavorites();
@@ -133,7 +90,6 @@ export default class Home extends React.Component {
       recentDropoff: getRecentDropoffs(),
       markers: getMarkers(),
     });
->>>>>>> parent of a9bd693 (AutoUpdate Locations)
     
   async reverseGeocode(latlong){
     let geocodeObj = await Location.reverseGeocodeAsync(latlong);
